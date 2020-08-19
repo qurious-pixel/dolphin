7461a8cdce98476b84c5fc35bbd92bb7ae0bbd74
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-19 07:41:50 -0700

    Update docker.sh

--- travis/appimage/docker.sh
+++ travis/appimage/docker.sh
@@ -1,17 +1,36 @@
 #!/bin/bash -ex
 
+QT_BASE_DIR=/opt/qt514
+export QTDIR=$QT_BASE_DIR
+export PATH=$QT_BASE_DIR/bin:$PATH
+export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
+
+ln -s /home/yuzu/.conan /root
+
 apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y libudev-dev libsfml-dev libminiupnpc-dev libmbedtls-dev libcurl4-openssl-dev libhidapi-dev libsystemd-dev libbluetooth-dev libpulse-dev libpugixml-dev libbz2-dev liblzo2-dev libpng-dev libusb-1.0-0-dev gettext \
+#qtbase5-private-dev libzstd-dev
+#curl -sLO "http://launchpadlibrarian.net/325446110/libusb-1.0-0-dev_1.0.21-2_amd64.deb"
+#curl -sLO "http://mirrors.kernel.org/ubuntu/pool/main/libu/libusb-1.0/libusb-1.0-0-dev_1.0.20-1_amd64.deb"
+#dpkg -i *.deb
 
 cd /dolphin
 
 mkdir build
 cd build
-cmake .. -G Ninja -DLINUX_LOCAL_DEV=true
+conan install .. -o zstd:shared=True --build missing
+cmake .. -G Ninja -DLINUX_LOCAL_DEV=true -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++
+#cmake .. -G Ninja
 ninja
-ln -s ../../Data/Sys Binaries/
+#ln -s ../../Data/Sys Binaries/
+
+cat /dolphin/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
 
 cd /tmp
-curl -sLO "https://raw.githubusercontent.com/qurious-pixel/dolphin/master/travis/appimage/appimage.sh"
-chmod a+x appimage.sh
-./appimage.sh
+#curl -sLO "https://raw.githubusercontent.com/qurious-pixel/dolphin/master/travis/appimage/appimage.sh"
+#chmod a+x appimage.sh
+#./appimage.sh
+ls -al /dolphin
+ls -al /dolphin/build
+ls -al /dolphin/build/Binaries
