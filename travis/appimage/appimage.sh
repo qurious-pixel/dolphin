a0db7b830263b2fa75ea7014b50684787f606e4e
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-19 13:24:03 -0700

    Update appimage.sh

--- travis/appimage/appimage.sh
+++ travis/appimage/appimage.sh
@@ -14,9 +14,8 @@ export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
 cd /tmp
 	curl -sLO "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-	#curl -sLO "https://github.com/qurious-pixel/yuzu/raw/master/.travis/appimage/crypto-libs.tar.gz"
-	#curl -sLO "https://github.com/qurious-pixel/yuzu/raw/master/.travis/appimage/update.tar.gz"
-	#tar -xzf update.tar.gz
+	curl -sLO "https://github.com/qurious-pixel/yuzu/raw/master/.travis/appimage/update.tar.gz"
+	tar -xzf update.tar.gz
 	#tar -xzf crypto-libs.tar.gz
 	chmod a+x linuxdeployqt*.AppImage
 ./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
@@ -26,17 +25,19 @@ cp -P "$BUILDBIN"/dolphin-emu $HOME/squashfs-root/usr/bin/
 
 curl -sL https://raw.githubusercontent.com/qurious-pixel/dolphin/master/Data/dolphin-emu.svg -o ./squashfs-root/dolphin-emu.svg
 curl -sL https://raw.githubusercontent.com/qurious-pixel/dolphin/master/Data/dolphin-emu.desktop -o ./squashfs-root/dolphin-emu.desktop
-curl -sL https://github.com/RPCS3/AppImageKit-checkrt/releases/download/continuous2/AppRun-patched-x86_64 -o ./squashfs-root/AppRun
+curl -sL https://github.com/RPCS3/AppImageKit-checkrt/releases/download/continuous2/AppRun-patched-x86_64 -o ./squashfs-root/AppRun-patched
 curl -sL https://github.com/AppImage/AppImageKit/releases/download/continuous/runtime-x86_64 -o ./squashfs-root/runtime
 mkdir -p squashfs-root/usr/share/applications && cp ./squashfs-root/dolphin-emu.desktop ./squashfs-root/usr/share/applications
 mkdir -p squashfs-root/usr/share/icons && cp ./squashfs-root/dolphin-emu.svg ./squashfs-root/usr/share/icons
 mkdir -p squashfs-root/usr/share/pixmaps && cp ./squashfs-root/dolphin-emu.svg ./squashfs-root/usr/share/pixmaps
-#mv /tmp/update/AppRun $HOME/squashfs-root/
-#mv /tmp/update/update.sh $HOME/squashfs-root/
+mv /tmp/update/AppRun $HOME/squashfs-root/
+mv /tmp/update/update.sh $HOME/squashfs-root/
 chmod a+x ./squashfs-root/runtime
 chmod a+x ./squashfs-root/AppRun
-#chmod a+x ./squashfs-root/update.sh
-#cp /tmp/libssl.so.47 /tmp/libcrypto.so.45 /usr/lib/x86_64-linux-gnu/
+chmod a+x ./squashfs-root/update.sh
+cp /tmp/libssl.so.47 /tmp/libcrypto.so.45 /usr/lib/x86_64-linux-gnu/
+printf "#include <bits/stdc++.h>\nint main(){std::make_exception_ptr(0);std::pmr::get_default_resource();}" | $CXX -x c++ -std=c++2a -o $HOME/squashfs-root/usr/optional/checker -
+
 
 echo $TRAVIS_COMMIT > $HOME/squashfs-root/version.txt
 
@@ -47,8 +48,8 @@ unset QTDIR
 # /tmp/squashfs-root/AppRun $HOME/squashfs-root/usr/bin/dolphin-emu -appimage -unsupported-allow-new-glibc -no-copy-copyright-files -no-translations -bundle-non-qt-libs
 /tmp/squashfs-root/AppRun $HOME/squashfs-root/usr/bin/dolphin-emu -unsupported-allow-new-glibc -no-copy-copyright-files -no-translations -bundle-non-qt-libs
 export PATH=$(readlink -f /tmp/squashfs-root/usr/bin/):$PATH
-#mv /tmp/update/AppImageUpdate $HOME/squashfs-root/usr/bin/
-#mv /tmp/update/* $HOME/squashfs-root/usr/lib/
+mv /tmp/update/AppImageUpdate $HOME/squashfs-root/usr/bin/
+mv /tmp/update/* $HOME/squashfs-root/usr/lib/
 /tmp/squashfs-root/usr/bin/appimagetool $HOME/squashfs-root -u "gh-releases-zsync|qurious-pixel|dolphin|continuous|dolphin-emu-x86_64.AppImage.zsync"
 
 mkdir $HOME/artifacts/
