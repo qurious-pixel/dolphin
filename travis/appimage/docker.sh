e05dd20171e14d5011681c732cd3a1065e3b4f11
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-19 10:53:40 -0700

    Update docker.sh

--- travis/appimage/docker.sh
+++ travis/appimage/docker.sh
@@ -12,26 +12,21 @@ apt-get update -y
 #DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y libudev-dev libsfml-dev libminiupnpc-dev libmbedtls-dev libcurl4-openssl-dev libhidapi-dev libsystemd-dev libbluetooth-dev libpulse-dev libpugixml-dev libbz2-dev liblzo2-dev libpng-dev libusb-1.0-0-dev gettext \
 #qtbase5-private-dev libzstd-dev
 DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y ca-certificates qtbase5-dev qtbase5-private-dev git cmake make gcc g++ pkg-config libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libxi-dev libxrandr-dev libudev-dev libevdev-dev libsfml-dev libminiupnpc-dev libmbedtls-dev libcurl4-openssl-dev libhidapi-dev libsystemd-dev libbluetooth-dev libasound2-dev libpulse-dev libpugixml-dev libbz2-dev libzstd-dev liblzo2-dev libpng-dev libusb-1.0-0-dev gettext
-#curl -sLO "http://launchpadlibrarian.net/325446110/libusb-1.0-0-dev_1.0.21-2_amd64.deb"
-#curl -sLO "http://mirrors.kernel.org/ubuntu/pool/main/libu/libusb-1.0/libusb-1.0-0-dev_1.0.20-1_amd64.deb"
-#dpkg -i *.deb
 
 cd /dolphin
 
 mkdir build
 cd build
-#conan install .. -o zstd:shared=True --build missing
 cmake .. -G Ninja -DLINUX_LOCAL_DEV=true -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++
-#cmake .. -G Ninja
 ninja
 #ln -s ../../Data/Sys Binaries/
 
 cat /dolphin/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
 
 cd /tmp
-#curl -sLO "https://raw.githubusercontent.com/qurious-pixel/dolphin/master/travis/appimage/appimage.sh"
-#chmod a+x appimage.sh
-#./appimage.sh
-ls -al /dolphin
-ls -al /dolphin/build
-ls -al /dolphin/build/Binaries
+curl -sLO "https://raw.githubusercontent.com/qurious-pixel/dolphin/master/travis/appimage/appimage.sh"
+chmod a+x appimage.sh
+./appimage.sh
+#ls -al /dolphin
+#ls -al /dolphin/build
+#ls -al /dolphin/build/Binaries
