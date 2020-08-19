77cb207e7b5485d49bd13962155996a32043c960
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-18 22:49:55 -0700

    Update docker.sh

--- travis/appimage/docker.sh
+++ travis/appimage/docker.sh
@@ -12,6 +12,6 @@ ninja
 ln -s ../../Data/Sys Binaries/
 
 cd /tmp
-curl -sLO "https://raw.githubusercontent.com/qurious-pixel/dolphin/zap/travis/appimage/appimage.sh"
+curl -sLO "https://raw.githubusercontent.com/qurious-pixel/dolphin/master/travis/appimage/appimage.sh"
 chmod a+x appimage.sh
 ./appimage.sh
