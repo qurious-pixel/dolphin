57dbcfce0935bacfabbf1d49cbc88dcc1ce8f597
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-19 17:48:39 -0700

    Update docker.sh

--- travis/appimage/docker.sh
+++ travis/appimage/docker.sh
@@ -3,24 +3,26 @@
 branch=$TRAVIS_BRANCH
 
 GCCVER=9
+GCC_BINARY=gcc-${GCCVER}
+GXX_BINARY=g++-${GCCVER}
 QT_BASE_DIR=/opt/qt514
 export QTDIR=$QT_BASE_DIR
 export PATH=$QT_BASE_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
-	update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCCVER} 10 && \
-	update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-${GCCVER} 10 && \
-  gcc --version && \
-	g++ --version 
-
 ln -s /home/yuzu/.conan /root
 
 apt-get update -y
 #DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y libudev-dev libsfml-dev libminiupnpc-dev libmbedtls-dev libcurl4-openssl-dev libhidapi-dev libsystemd-dev libbluetooth-dev libpulse-dev libpugixml-dev libbz2-dev liblzo2-dev libpng-dev libusb-1.0-0-dev gettext \
 #qtbase5-private-dev libzstd-dev
-DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y liblzma-dev libsfml-dev libminiupnpc-dev libmbedtls-dev libcurl4-openssl-dev libhidapi-dev libsystemd-dev libbluetooth-dev libpugixml-dev libbz2-dev liblzo2-dev libpng-dev gettext
+DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y $GCC_BINARY $GXX_BINARY libsfml-dev libminiupnpc-dev libmbedtls-dev libcurl4-openssl-dev libhidapi-dev libsystemd-dev libbluetooth-dev libpugixml-dev libbz2-dev liblzo2-dev libpng-dev gettext
 
+	update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCCVER} 20 
+	update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-${GCCVER} 20 
+  gcc --version 
+	g++ --version 
+	
 cd /dolphin
 
 mkdir build
