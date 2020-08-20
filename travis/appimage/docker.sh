aeb39618776b15e378f4a98e42dbbf4cb013370a
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-19 18:42:30 -0700

    Update docker.sh

--- travis/appimage/docker.sh
+++ travis/appimage/docker.sh
@@ -12,7 +12,7 @@ export PATH=$QT_BASE_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
-ln -s /home/yuzu/.conan /root
+ln -s $HOME/.conan /root
 
 apt-get update -y
 apt-get install -y curl software-properties-common apt-transport-https
