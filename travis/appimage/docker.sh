77fc06c7b748c0069e5be5c42d49377bb6762a8b
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-26 11:10:22 -0700

    Update docker.sh

--- travis/appimage/docker.sh
+++ travis/appimage/docker.sh
@@ -6,16 +6,21 @@ export PATH=$QT_BASE_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
-apt-get update
-apt-get install -y libglvnd-dev 
+CMAKEVER=3.18.1
+
+	cd /tmp && \
+	curl -sLO https://cmake.org/files/v${CMAKEVER%.*}/cmake-${CMAKEVER}-Linux-x86_64.sh 
+	sh cmake-${CMAKEVER}-Linux-x86_64.sh --prefix=/usr --skip-license 
+	rm ./cmake*.sh 
+	cmake --version
 
 ln -s /home/yuzu/.conan /root
 
-cd dolphin/
+cd /dolphin
 
 git clone https://github.com/dolphin-emu/dolphin.git
 git submodule update --init --recursive
-cd /dolphin
+cd dolphin/
 
 mkdir build
 cd build
