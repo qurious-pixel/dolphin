6822b93cc83fd30069f068735b9f1b60a16e4696
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-27 10:00:36 -0700

    Create update.sh

---
+++ travis/appimage/update.sh
@@ -0,0 +1,14 @@
+  
+#!/bin/bash
+
+$APPDIR/usr/bin/zenity --question --timeout=10 --title="dolphin updater" --text="New update available. Update now?" --icon-name=dolphin-emu --window-icon=dolphin-emu.svg --height=80 --width=400
+answer=$?
+
+if [ "$answer" -eq 0 ]; then 
+	$APPDIR/usr/bin/AppImageUpdate $PWD/Dolphin_Emulator-x86_64.AppImage && $PWD/Dolphin_Emulator-x86_64.AppImage
+elif [ "$answer" -eq 1 ]; then
+	QT_QPA_PLATFORM=xcb $APPDIR/usr/bin/dolphin-emu
+elif [ "$answer" -eq 5 ]; then
+	QT_QPA_PLATFORM=xcb $APPDIR/usr/bin/dolphin-emu
+fi
+exit 0
