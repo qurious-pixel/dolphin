0bd75e1999fa0abf95790abb18bee6c8fc9c9480
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-27 09:48:38 -0700

    Update docker.sh

--- travis/appimage/docker.sh
+++ travis/appimage/docker.sh
@@ -10,21 +10,6 @@ export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
 ln -s /home/yuzu/.conan /root
 
-cd /tmp
-git clone https://github.com/facebook/zstd.git
-cd zstd/build/cmake
-mkdir build && cd build
-cmake .. -GNinja
-ninja && ninja install
-
-CMAKEVER=3.18.1
-
-	cd /tmp 
-	curl -sLO https://cmake.org/files/v${CMAKEVER%.*}/cmake-${CMAKEVER}-Linux-x86_64.sh 
-	sh cmake-${CMAKEVER}-Linux-x86_64.sh --prefix=/usr --skip-license 
-	rm ./cmake*.sh 
-	cmake --version
-
 cd /dolphin
 
 git clone https://github.com/dolphin-emu/dolphin.git
@@ -37,7 +22,7 @@ cmake .. -G Ninja -DLINUX_LOCAL_DEV=true -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc 
 ninja
 #ln -s ../../Data/Sys Binaries/
 
-cat /dolphin/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
+#cat /dolphin/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
 
 cd /tmp
 curl -sLO "https://raw.githubusercontent.com/qurious-pixel/dolphin/$branch/travis/appimage/appimage.sh"
