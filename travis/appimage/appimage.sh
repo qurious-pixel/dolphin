4965258b162a94ec4e640044dafa1ffa0d8a18d6
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-06-21 13:04:19 -0700

    dolphin-emu
    dolphin-emu & dolphin-emu-nogui

--- travis/appimage/appimage.sh
+++ travis/appimage/appimage.sh
@@ -76,7 +76,7 @@ mkdir $HOME/artifacts/
 mkdir -p /dolphin/artifacts/
 mv Dolphin_Emulator-x86_64.AppImage* $HOME/artifacts
 cp -R $HOME/artifacts/ /dolphin/
-cp $BUILDBIN/dolphin-emu* /dolphin/artifacts
+cp $BUILDBIN/{dolphin-emu,dolphin-emu-nogui} /dolphin/artifacts
 chmod -R 777 /dolphin/artifacts
 cd /dolphin/artifacts
 ls -al /dolphin/artifacts/
