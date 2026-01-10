74507fae32203fea8e6bd7a8d8643ec3b0b14e1f
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2026-01-10 10:13:06 -0800

    Update HookableEvent.h

--- Source/Core/Common/HookableEvent.h
+++ Source/Core/Common/HookableEvent.h
@@ -88,6 +88,8 @@ public:
 private:
   struct Listener
   {
+    explicit Listener(CallbackType cb) : callback(std::move(cb)) {}
+
     const CallbackType callback;
     bool is_pending_removal{};
   };
